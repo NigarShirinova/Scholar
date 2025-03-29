00c0b1ca1eb02807bd3854b404ad1c7a3bce7bfa
Nigar Shirinova <nigaresh@code.edu.az>
2025-03-29 17:51:01 +0400

    dockerfile corrected

--- Common/obj/Debug/net9.0/Common.AssemblyInfo.cs
+++ Common/obj/Debug/net9.0/Common.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Common")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b2f9c293c8dea780e283d1234a51ec036c172511")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a63582b1c1a82dc20c770dabad9bb8c10cef3074")]
 [assembly: System.Reflection.AssemblyProductAttribute("Common")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Common")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
