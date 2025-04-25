bfe402bd0d25a5480e6b432df56e0bc8e67aeea0
Nigar Shirinova <nigaresh@code.edu.az>
2025-04-26 00:53:25 +0400

    finished.

--- Business/obj/Debug/net9.0/Business.AssemblyInfo.cs
+++ Business/obj/Debug/net9.0/Business.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Business")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+771bc3209579701bbf9e9e4d6b7ad4e1ab919eaf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1d8d59cc1348518ddd78456334ddc958c67ec529")]
 [assembly: System.Reflection.AssemblyProductAttribute("Business")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Business")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Presentation/obj/Debug/net9.0/Presentation.AssemblyInfo.cs
+++ Presentation/obj/Debug/net9.0/Presentation.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Presentation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+771bc3209579701bbf9e9e4d6b7ad4e1ab919eaf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1d8d59cc1348518ddd78456334ddc958c67ec529")]
 [assembly: System.Reflection.AssemblyProductAttribute("Presentation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Presentation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
