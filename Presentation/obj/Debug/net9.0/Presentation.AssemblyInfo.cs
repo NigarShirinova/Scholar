0c61c400ee0ebb7860f0694f0a8389818208a0e9
Nigar Shirinova <nigaresh@code.edu.az>
2025-04-25 23:00:03 +0400

    final

--- Presentation/obj/Debug/net9.0/Presentation.AssemblyInfo.cs
+++ Presentation/obj/Debug/net9.0/Presentation.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Presentation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+87775c0fff2c03c53c64e33aea90bfa0c20a1ffa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+442f186ca2d8fca095df0dfa464ac52ac45ef063")]
 [assembly: System.Reflection.AssemblyProductAttribute("Presentation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Presentation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
